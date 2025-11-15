bd02ad610e679ab308f96623cf21dd09f73290f5
Burak Ilgin <burakilginnn@gmail.com>
2025-11-15 21:05:38 +0300

    Initial Commit

---
+++ SwiftUIProject2/SwiftUIProject2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProject2App.swift
+//  SwiftUIProject2
+//
+//  Created by Burak Ilgın on 15.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
